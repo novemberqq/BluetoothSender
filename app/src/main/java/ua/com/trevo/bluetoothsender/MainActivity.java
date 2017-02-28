d2b81027a2fdb22b1c995b9414906166524da9ca
novemberqq <novemberqq@gmail.com>
2017-03-01 01:42:57 +0200

    Work_interface Bluetooth_try Bluetooth_ok

--- app/src/main/java/ua/com/trevo/bluetoothsender/MainActivity.java
+++ app/src/main/java/ua/com/trevo/bluetoothsender/MainActivity.java
@@ -2,42 +2,40 @@ package ua.com.trevo.bluetoothsender;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TabHost;
+import java.util.UUID;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
+
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener{
 
-    ToggleButton PWM1;
-    ToggleButton PWM2;
-    ToggleButton PWM3;
-    Switch SW1;
-    Switch SW2;
-    Switch SW3;
-    Button SendFromTerminal;
-    Button ConnectToDevice;
-    EditText DeviceIdEdit;
+    private ToggleButton PWM1;
+    private ToggleButton PWM2;
+    private ToggleButton PWM3;
+    private Switch SW1;
+    private Switch SW2;
+    private Switch SW3;
+    private Button SendFromTerminal;
+    private EditText TextToSend;
 
-    BluetoothSocket clientSocket;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static String address = "20:16:11:23:99:51";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
@@ -71,86 +69,120 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         PWM1 = (ToggleButton) findViewById(R.id.toggleButton1);
         PWM2 = (ToggleButton) findViewById(R.id.toggleButton2);
         PWM3 = (ToggleButton) findViewById(R.id.toggleButton3);
+
         SW1 = (Switch) findViewById(R.id.switch1);
         SW2 = (Switch) findViewById(R.id.switch2);
         SW3 = (Switch) findViewById(R.id.switch3);
-        DeviceIdEdit = (EditText) findViewById(R.id.deviceID);
+
+        TextToSend = (EditText) findViewById(R.id.editSend);
 
         SendFromTerminal = (Button) findViewById(R.id.sendTerminal);
-        ConnectToDevice = (Button) findViewById(R.id.connectDevice);
-
 
         PWM1.setOnClickListener(this); SW1.setOnClickListener(this);
         PWM2.setOnClickListener(this); SW2.setOnClickListener(this);
         PWM3.setOnClickListener(this); SW3.setOnClickListener(this);
         SendFromTerminal.setOnClickListener(this);
-        ConnectToDevice.setOnClickListener(this);
 
-        String enableBT = BluetoothAdapter.ACTION_REQUEST_ENABLE;
-        startActivityForResult(new Intent(enableBT), 0);
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(!btAdapter.isEnabled()) {
+            String enableBT = BluetoothAdapter.ACTION_REQUEST_ENABLE;
+            startActivityForResult(new Intent(enableBT), 0);
+        }
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        try {
+            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException ignored) {
+        }
+
+        btAdapter.cancelDiscovery();
+
+        try {
+            btSocket.connect();
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException ignored) {
+            }
+        }
+
+        try {
+            outStream = btSocket.getOutputStream();
+        } catch (IOException ignored) {
+
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (outStream != null) {
+            try {
+                outStream.flush();
+            } catch (IOException ignored) {
+
+            }
+        }
+
+        try     {
+            btSocket.close();
+        } catch (IOException ignored) {
+
+        }
+    }
+
+    private void sendData(String message) {
+        byte[] msgBuffer = message.getBytes();
+
+        try {
+            outStream.write(msgBuffer);
+        } catch (IOException ignored) {
+
+        }
     }
 
     @Override
     public void onClick(View v) {
 
-            try {
-                OutputStream outStream = clientSocket.getOutputStream();
+        String value = "#";
 
-                int value = 0;
-
-                if(v == SW1){
-                    value = (SW1.isChecked() ? 1 : 0) + 2;
-                }else if(v == SW2){
-                    value = (SW2.isChecked() ? 1 : 0) + 4;
-                }else if(v == SW3){
-                    value = (SW3.isChecked() ? 1 : 0) + 8;
-                }else if (v == PWM1) {
-                    value = (PWM1.isChecked() ? 1 : 0) + 16;
-                } else if (v == PWM2) {
-                    value = (PWM2.isChecked() ? 1 : 0) + 32;
-                } else if (v == PWM3) {
-                    value = (PWM3.isChecked() ? 1 : 0) + 64;
-                } else if(v == SendFromTerminal){
-
-                } else if(v == ConnectToDevice) {
-
-                    BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
-
-                    Editable str  = DeviceIdEdit.getText();
-
-                    try{
-                        BluetoothDevice device = bluetooth.getRemoteDevice(str.toString());
-
-                        Method m = device.getClass().getMethod(
-                                "createRfcommSocket", int.class);
-
-                        clientSocket = (BluetoothSocket) m.invoke(device, 1);
-                        clientSocket.connect();
-
-                        //В случае появления любых ошибок, выводим в лог сообщение
-                    } catch (IOException e) {
-                        Log.d("BLUETOOTH", e.getMessage());
-                    } catch (SecurityException e) {
-                        Log.d("BLUETOOTH", e.getMessage());
-                    } catch (NoSuchMethodException e) {
-                        Log.d("BLUETOOTH", e.getMessage());
-                    } catch (IllegalArgumentException e) {
-                        Log.d("BLUETOOTH", e.getMessage());
-                    } catch (IllegalAccessException e) {
-                        Log.d("BLUETOOTH", e.getMessage());
-                    } catch (InvocationTargetException e) {
-                        Log.d("BLUETOOTH", e.getMessage());
-                    }
-
-                    //Выводим сообщение об успешном подключении
-                    Toast.makeText(getApplicationContext(), "CONNECTED", Toast.LENGTH_LONG).show();
-                }
-
-                outStream.write(value);
-
-            } catch (IOException e) {
-                Log.d("BLUETOOTH", e.getMessage());
-            }
+        if(v == SW1) {
+            if(SW1.isChecked()){value += "S1+";}
+            else {value += "S1-";}
         }
-
+        else if(v == SW2) {
+            if(SW2.isChecked()){value += "S2+";}
+            else {value += "S2-";}
+        }
+        else if(v == SW3) {
+            if(SW3.isChecked()){value += "S3+";}
+            else {value += "S3-";}
+        }
+        else if(v == PWM1) {
+            if (PWM1.isChecked()) {value += "P1+";}
+            else {value += "P1-";}
+        }
+        else if(v == PWM2) {
+            if (PWM2.isChecked()) {value += "P2+";}
+            else {value += "P2-";}
+        }
+        else if(v == PWM3) {
+            if (PWM3.isChecked()) {value += "P3+";}
+            else {value += "P3-";}
+        }
+        value += "^";
+        if(v == SendFromTerminal) {
+            value = TextToSend.getText().toString();
+        }
+        sendData(value);
+    }
 }
